e32018ad563947d87395a5782cc1d7f62896248f
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-03-03 20:08:17 +0100

    define a "transaction" with try-catch

--- src/main/java/exceptionsubchapter/Demo.java
+++ src/main/java/exceptionsubchapter/Demo.java
@@ -1,10 +1,46 @@
 package exceptionsubchapter;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class FileObject implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    String name;
+    String surname;
+
+    // ...
+
+
+    @Override
+    public String toString() {
+        return "FileObject{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
+
 public class Demo {
 
     public static void main(String[] args) {
 
     }
 
-    
+    public static FileObject retreiveObject(String fileName) {
+        try {
+            FileInputStream fileInStream = new FileInputStream(fileName);
+            ObjectInputStream objectInStream = new ObjectInputStream(fileInStream);
+
+            FileObject fileObject = (FileObject) objectInStream.readObject();
+            objectInStream.close();
+
+            return fileObject;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
