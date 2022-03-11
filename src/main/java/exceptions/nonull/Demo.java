7e009997c336f54a973fe6f4a27176d969de960b
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-03-11 18:20:55 +0100

    improve example

--- src/main/java/exceptions/nonull/Demo.java
+++ src/main/java/exceptions/nonull/Demo.java
@@ -40,13 +40,25 @@ public class Demo {
     }
 
     public double operationOnPoints(Point p1, Point p2) {
+        return p2.x - p1.x;
+    }
+
+    public double operationOnPoints1(Point p1, Point p2) {
         if (p1 == null || p2 == null) {
-            throw new IllegalArgumentException("...");
+            throw new IllegalArgumentException("Point cannot be null");
         }
+        return p2.x - p1.x;
+    }
+
+    public double operationOnPoints2(Point p1, Point p2) {
+        assert p1 != null : "p1 can't be null";
+        assert p2 != null : "p2 can't be null";
         return p2.x - p1.x;
     }
 
     public static void main(String[] args) {
         final double v = new Demo().operationOnPoints(new Point(), new Point());
+        final double v1 = new Demo().operationOnPoints1(new Point(), new Point());
+        final double v2 = new Demo().operationOnPoints2(new Point(), new Point());
     }
 }
